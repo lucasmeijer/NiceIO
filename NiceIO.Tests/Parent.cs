dc22eaf0c91e51e68cd829913b5d9da74df94222
Lucas Meijer <lucas@lucasmeijer.com>
2015-03-29 05:51:57 -0700

    move everything in the implementation around to have some logical grouping.  introduce regions.  oh my god, I'm using regions, what has happened.

---
+++ NiceIO.Tests/Parent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NiceIO.Tests
+{
+	[TestFixture]
+	public class Parent
+	{
+		[Test]
+		public void ParentFromFileInDirectory()
+		{
+			var path = new Path("mydir/myotherdir/myfile.exe").Parent();
+			Assert.AreEqual("mydir/myotherdir", path.ToString());
+		}
+
+		[Test]
+		public void ParentFromFile()
+		{
+			var path = new Path("myfile.exe").Parent();
+			Assert.AreEqual("", path.ToString());
+		}
+
+		[Test]
+		public void ParentFromFileInRoot()
+		{
+			var path = new Path("/myfile.exe").Parent();
+			Assert.AreEqual("/", path.ToString());
+		}
+
+		[Test]
+		public void ParentFromEmpty()
+		{
+			Assert.Throws<InvalidOperationException>(() => new Path("/").Parent());
+		}
+	}
+}
--- NiceIO.cs
+++ NiceIO.cs
@@ -12,6 +12,9 @@ namespace NiceIO
 		private readonly bool _isRelative;
 		private string _driveLetter;
 
+
+#region construction
+		
 		public Path(string path)
 		{
 			path = ParseDriveLetter(path);
@@ -30,6 +33,22 @@ namespace NiceIO
 			_driveLetter = driveLetter;
 		}
 
+		public Path Combine(string append)
+		{
+			return Combine(new Path(append));
+		}
+
+		public Path Combine(Path toAppend)
+		{
+			if (!toAppend.IsRelative)
+				throw new ArgumentException("You cannot .Combine a non-relative path");
+
+			return new Path(_elements.Concat(toAppend._elements).ToArray(), _isRelative, _driveLetter);
+		}
+#endregion construction
+
+#region inspection
+
 		public bool IsRelative
 		{
 			get { return _isRelative; }
@@ -38,6 +57,21 @@ namespace NiceIO
 		public string FileName
 		{
 			get { return _elements.Last(); }
+		}
+
+		public bool Exists()
+		{
+			return FileExists() || DirectoryExists();
+		}
+
+		public bool DirectoryExists()
+		{
+			return Directory.Exists(ToString());
+		}
+
+		public bool FileExists()
+		{
+			return File.Exists(ToString());
 		}
 
 		public string ExtensionWithDot
@@ -50,6 +84,205 @@ namespace NiceIO
 				return last.Substring(index);
 			}
 		}
+
+		public Path Parent()
+		{
+			if (_elements.Length == 0)
+				throw new InvalidOperationException("Parent() is called on an empty path");
+
+			var newElements = _elements.Take(_elements.Length - 1).ToArray();
+
+			return new Path(newElements, _isRelative, _driveLetter);
+		}
+
+		public Path RelativeTo(Path path)
+		{
+			if (!IsBelowOrEqual(path))
+				throw new ArgumentException("Path.RelativeTo() was invoked with two paths that are unrelated. invoked on: " + ToString() + " asked to be made relative to: " + path);
+
+			return new Path(_elements.Skip(path._elements.Length).ToArray(), true, null);
+		}
+
+
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			if (_driveLetter != null)
+			{
+				sb.Append(_driveLetter);
+				sb.Append(":");
+			}
+			if (!_isRelative)
+				sb.Append("/");
+			var first = true;
+			foreach (var element in _elements)
+			{
+				if (!first)
+					sb.Append("/");
+
+				sb.Append(element);
+				first = false;
+			}
+			return sb.ToString();
+		}
+
+		public override bool Equals(Object obj)
+		{
+			if (obj == null)
+				return false;
+
+			// If parameter cannot be cast to Point return false.
+			var p = obj as Path;
+			if ((Object)p == null)
+				return false;
+
+			if (p._isRelative != _isRelative)
+				return false;
+			if (p._driveLetter != _driveLetter)
+				return false;
+
+			if (p._elements.Length != _elements.Length)
+				return false;
+
+			for (var i = 0; i != _elements.Length; i++)
+				if (p._elements[i] != _elements[i])
+					return false;
+
+			return true;
+		}
+
+		public static bool operator ==(Path a, Path b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (ReferenceEquals(a, b))
+				return true;
+
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+				return false;
+
+			// Return true if the fields match:
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(Path a, Path b)
+		{
+			return !(a == b);
+		}
+
+		public bool HasExtension(string extension)
+		{
+			var withDot = extension.StartsWith(".") ? extension : "." + extension;
+			return withDot == ExtensionWithDot;
+		}
+
+		private bool IsEmpty()
+		{
+			return _elements.Length == 0;
+		}
+#endregion inspection	
+
+#region directory enumeration
+
+		public IEnumerable<Path> Files(SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			return Directory.GetFiles(ToString(), "*", searchOption).Select(s => new Path(s));
+		}
+
+		public IEnumerable<Path> Files(Func<Path, bool> filter)
+		{
+			return Files().Where(filter);
+		}
+
+		public IEnumerable<Path> Contents(SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			return Files(searchOption).Concat(Directories(searchOption));
+		}
+
+		public IEnumerable<Path> Directories(SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			return Directory.GetDirectories(ToString(), "*", searchOption).Select(s => new Path(s));
+		}
+
+#endregion
+
+#region filesystem writing operations
+		public Path CreateFile()
+		{
+			EnsureDirectoryExists(Parent());
+			File.WriteAllBytes(ToString(), new byte[0]);
+			return this;
+		}
+
+		public Path CreateDirectory()
+		{
+			Directory.CreateDirectory(ToString());
+			return this;
+		}
+
+		public void Copy(Path dest)
+		{
+			Copy(dest,p => true);
+		}
+
+		public void Copy(Path dest, Func<Path,bool> filter)
+		{
+			ThrowIfRelative();
+			if (dest.IsRelative)
+				throw new InvalidOperationException("Cannot copy to a relative path");
+
+			if (!filter(dest))
+				return;
+
+			if (FileExists())
+			{
+				EnsureDirectoryExists(dest.Parent());
+				File.Copy(ToString(), dest.ToString(), true);
+			}
+			else if (DirectoryExists())
+			{
+				EnsureDirectoryExists(dest);
+				foreach (var thing in Contents())
+					thing.Copy(dest.Combine(thing.RelativeTo(this)),filter);
+			}
+			else
+			{
+				throw new ArgumentException("Copy() called on path that doesnt exist: " + ToString());
+			}
+		}
+
+		public void Delete(DeleteMode deleteMode = DeleteMode.Normal)
+		{
+			ThrowIfRelative();
+
+			if (FileExists())
+				File.Delete(ToString());
+			else if (DirectoryExists())
+				try
+				{
+					Directory.Delete(ToString(), true);
+				}
+				catch (IOException)
+				{
+					if (deleteMode == DeleteMode.Normal)
+						throw;
+				}
+			else
+				throw new InvalidOperationException("Trying to delete a path that does not exist: " + ToString());
+		}
+
+		public static Path CreateTempDirectory(string myprefix)
+		{
+			var random = new Random();
+			while (true)
+			{
+				var candidate = new Path(System.IO.Path.GetTempPath() + "/" + myprefix + "_" + random.Next());
+				if (!candidate.Exists())
+					return candidate.CreateDirectory();
+			}
+		}
+		#endregion
 
 		private string ParseDriveLetter(string path)
 		{
@@ -75,153 +308,18 @@ namespace NiceIO
 			return path.Split('/', '\\');
 		}
 
-		public override string ToString()
-		{
-			var sb = new StringBuilder();
-			if (_driveLetter != null)
-			{
-				sb.Append(_driveLetter);
-				sb.Append(":");
-			}
-			if (!_isRelative)
-				sb.Append("/");
-			var first = true;
-			foreach (var element in _elements)
-			{
-				if (!first)
-					sb.Append("/");
-
-				sb.Append(element);
-				first = false;
-			}
-			return sb.ToString();
-		}
-
-		public Path Up()
-		{
-			if (_elements.Length == 0)
-				throw new InvalidOperationException("Up() is called on an empty path");
-
-			var newElements = _elements.Take(_elements.Length - 1).ToArray();
-
-			return new Path(newElements, _isRelative, _driveLetter);
-		}
-
-		public static Path CreateTempDirectory(string myprefix)
-		{
-			var random = new Random();
-			while (true)
-			{
-				var candidate = new Path(System.IO.Path.GetTempPath() + "/" + myprefix + "_" + random.Next());
-				if (!candidate.Exists())
-					return candidate.CreateDirectory();
-			}
-		}
-
-		public bool Exists()
-		{
-			return FileExists() || DirectoryExists();
-		}
-
-		public bool DirectoryExists()
-		{
-			return Directory.Exists(ToString());
-		}
-
-		public bool FileExists()
-		{
-			return File.Exists(ToString());
-		}
-
-		public Path CreateDirectory()
-		{
-			Directory.CreateDirectory(ToString());
-			return this;
-		}
-
-		public Path Combine(string append)
-		{
-			return Combine(new Path(append));
-		}
-
-		public void Delete(DeleteMode deleteMode = DeleteMode.Normal)
-		{
-			ThrowIfRelative();
-
-			if (FileExists())
-				File.Delete(ToString());
-			else if (DirectoryExists())
-				try
-				{
-					Directory.Delete(ToString(), true);
-				}
-				catch (IOException)
-				{
-					if (deleteMode == DeleteMode.Normal)
-						throw;
-				}
-			else
-				throw new InvalidOperationException("Trying to delete a path that does not exist: " + ToString());
-		}
-
 		private void ThrowIfRelative()
 		{
 			if (_isRelative)
 				throw new InvalidOperationException("You are attempting an operation on a Path that requires an absolute path, but the path is relative");
 		}
 
-		public Path CreateFile()
-		{
-			EnsureDirectoryExists(Up());
-			File.WriteAllBytes(ToString(), new byte[0]);
-			return this;
-		}
-
 		private void EnsureDirectoryExists(Path directory)
 		{
 			if (directory.DirectoryExists())
 				return;
-			EnsureDirectoryExists(directory.Up());
+			EnsureDirectoryExists(directory.Parent());
 			directory.CreateDirectory();
-		}
-
-		public void Copy(Path dest)
-		{
-			Copy(dest,p => true);
-		}
-
-		public void Copy(Path dest, Func<Path,bool> filter)
-		{
-			ThrowIfRelative();
-			if (dest.IsRelative)
-				throw new InvalidOperationException("Cannot copy to a relative path");
-
-			if (!filter(dest))
-				return;
-
-			if (FileExists())
-			{
-				EnsureDirectoryExists(dest.Up());
-				File.Copy(ToString(), dest.ToString(), true);
-			}
-			else if (DirectoryExists())
-			{
-				EnsureDirectoryExists(dest);
-				foreach (var thing in Contents())
-					thing.Copy(dest.Combine(thing.RelativeTo(this)),filter);
-			}
-			else
-			{
-				throw new ArgumentException("Copy() called on path that doesnt exist: " + ToString());
-			}
-		}
-
-		public Path RelativeTo(Path path)
-		{
-			if (!IsBelowOrEqual(path))
-				throw new ArgumentException("Path.RelativeTo() was invoked with two paths that are unrelated. invoked on: " + ToString() + " asked to be made relative to: " + path);
-
-			return new Path(_elements.Skip(path._elements.Length).ToArray(), true, null);
 		}
 
 		private bool IsBelowOrEqual(Path potentialBasePath)
@@ -232,90 +330,7 @@ namespace NiceIO
 			if (Equals(potentialBasePath))
 				return true;
 
-			return Up().IsBelowOrEqual(potentialBasePath);
-		}
-
-		private bool IsEmpty()
-		{
-			return _elements.Length == 0;
-		}
-
-		public IEnumerable<Path> Files(SearchOption searchOption = SearchOption.TopDirectoryOnly)
-		{
-			return Directory.GetFiles(ToString(), "*", searchOption).Select(s => new Path(s));
-		}
-
-		public IEnumerable<Path> Files(Func<Path, bool> filter)
-		{
-			return Files().Where(filter);
-		}
-
-		public IEnumerable<Path> Contents(SearchOption searchOption = SearchOption.TopDirectoryOnly)
-		{
-			return Files(searchOption).Concat(Directories(searchOption));
-		}
-
-		public IEnumerable<Path> Directories(SearchOption searchOption = SearchOption.TopDirectoryOnly)
-		{
-			return Directory.GetDirectories(ToString(), "*", searchOption).Select(s => new Path(s));
-		}
-
-		public override bool Equals(Object obj)
-		{
-			if (obj == null)
-				return false;
-
-			// If parameter cannot be cast to Point return false.
-			var p = obj as Path;
-			if ((Object) p == null)
-				return false;
-
-			if (p._isRelative != _isRelative)
-				return false;
-			if (p._driveLetter != _driveLetter)
-				return false;
-
-			if (p._elements.Length != _elements.Length)
-				return false;
-
-			for (var i = 0; i != _elements.Length; i++)
-				if (p._elements[i] != _elements[i])
-					return false;
-
-			return true;
-		}
-
-		public static bool operator ==(Path a, Path b)
-		{
-			// If both are null, or both are same instance, return true.
-			if (ReferenceEquals(a, b))
-				return true;
-
-			// If one is null, but not both, return false.
-			if (((object) a == null) || ((object) b == null))
-				return false;
-
-			// Return true if the fields match:
-			return a.Equals(b);
-		}
-
-		public static bool operator !=(Path a, Path b)
-		{
-			return !(a == b);
-		}
-
-		public Path Combine(Path toAppend)
-		{
-			if (!toAppend.IsRelative)
-				throw new ArgumentException("You cannot .Combine a non-relative path");
-
-			return new Path(_elements.Concat(toAppend._elements).ToArray(), _isRelative, _driveLetter);
-		}
-
-		public bool HasExtension(string extension)
-		{
-			var withDot = extension.StartsWith(".") ? extension : "." + extension;
-			return withDot == ExtensionWithDot;
+			return Parent().IsBelowOrEqual(potentialBasePath);
 		}
 	}
 
