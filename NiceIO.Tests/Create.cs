0e56297ad7ecc4dc252ea27b1e507587929bf220
Lucas Meijer <lucas@lucasmeijer.com>
2015-03-29 06:00:44 -0700

    tests for CreateDirectory

---
+++ NiceIO.Tests/Create.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NiceIO.Tests
+{
+	[TestFixture]
+	public class Create : TestWithTempDir
+	{
+		[Test]
+		public void File_SimpleCase()
+		{
+			Assert.IsTrue(_tempPath.Combine("myfile").CreateFile().FileExists());
+		}
+
+		[Test]
+		public void File_InNonExistingDirectory()
+		{
+			_tempPath.Combine("not_yet_existing_dir/myotherdir/myfile").CreateFile().FileExists();
+		}
+
+		[Test]
+		public void File_OnRelativePath()
+		{
+			Assert.Throws<InvalidOperationException>(() => new Path("mydir/myfile.txt").CreateFile());
+		}
+
+		[Test]
+		public void Directory_SimpleCase()
+		{
+			Assert.IsTrue(_tempPath.Combine("mydir").CreateDirectory().FileExists());
+		}
+
+		[Test]
+		public void Directory_InNonExistingDirectory()
+		{
+			_tempPath.Combine("not_yet_existing_dir/myotherdir/mydir").CreateDirectory().DirectoryExists();
+		}
+
+		[Test]
+		public void Directory_OnRelativePath()
+		{
+			Assert.Throws<InvalidOperationException>(() => new Path("mydir/mydir2").CreateDirectory());
+		}
+	}
+}
