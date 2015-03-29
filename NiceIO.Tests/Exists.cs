15e21f9dea6835dcb6da0ef26c7b4a979f3d94a6
Lucas Meijer <lucas@lucasmeijer.com>
2015-03-29 01:30:21 -0700

    make tempdir parts of exist tests reusable for future tests

--- NiceIO.Tests/Exists.cs
+++ NiceIO.Tests/Exists.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace NiceIO.Tests
 {
 	[TestFixture]
-	public class Exists
+	public class Exists : TestWithTempDir
 	{
-		private DeleteOnDisposePath _tempPath;
-
-		[SetUp]
-		public void Setup()
-		{
-			_tempPath = Path.CreateTempDirectory("Exists");
-		}
-
-		[TearDown]
-		public void TearDown()
-		{
-			_tempPath.Dispose();
-		}
-
 		[Test]
 		public void FileExists()
 		{
@@ -40,17 +21,6 @@ namespace NiceIO.Tests
 			Assert.IsTrue(_tempPath.Combine("somefile").Exists());
 			Assert.IsFalse(_tempPath.Combine("somefile").FileExists());
 			Assert.IsTrue(_tempPath.Combine("somefile").DirectoryExists());
-		}
-
-		private void PopulateTempDir(string[] entries)
-		{
-			foreach (var entry in entries)
-			{
-				if (entry[entry.Length - 1] == '/')
-					Directory.CreateDirectory(_tempPath + "/" + entry);
-				else
-					File.WriteAllText(_tempPath+"/"+entry,"hello");
-			}
 		}
 	}
 }
---
+++ NiceIO.Tests/TestWithTempDir.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace NiceIO.Tests
+{
+	public class TestWithTempDir
+	{
+		protected DeleteOnDisposePath _tempPath;
+
+		[SetUp]
+		public void Setup()
+		{
+			_tempPath = Path.CreateTempDirectory("Exists");
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_tempPath.Dispose();
+		}
+
+		protected void PopulateTempDir(string[] entries)
+		{
+			foreach (var entry in entries)
+			{
+				if (entry[entry.Length - 1] == '/')
+					Directory.CreateDirectory(_tempPath + "/" + entry);
+				else
+					File.WriteAllText(_tempPath+"/"+entry,"hello");
+			}
+		}
+	}
+}
