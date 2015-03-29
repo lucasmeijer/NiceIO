ba822967c418106002f25b80155a74100ee02d08
Lucas Meijer <lucas@lucasmeijer.com>
2015-03-29 04:10:29 -0700

    more equality tests

--- NiceIO.Tests/Equality.cs
+++ NiceIO.Tests/Equality.cs
@@ -17,5 +17,41 @@ namespace NiceIO.Tests
 			Assert.IsTrue(path1.Equals(path2));
 			Assert.IsTrue(path1 == path2);
 		}
+
+		[Test]
+		public void InFolderButDifferentSlashes()
+		{
+			var path1 = new Path(@"mydir/myfile");
+			var path2 = new Path(@"mydir\myfile");
+			Assert.IsTrue(path1.Equals(path2));
+			Assert.IsTrue(path1 == path2);
+		}
+
+		[Test]
+		public void WithDifferentDriveLetters()
+		{
+			var path1 = new Path(@"c:\mydir\myfile");
+			var path2 = new Path(@"e:\mydir\myfile");
+			Assert.IsFalse(path1.Equals(path2));
+			Assert.IsFalse(path1 == path2);
+		}
+
+		[Test]
+		public void OneRootedOneNonRooted()
+		{
+			var path1 = new Path(@"\mydir\myfile");
+			var path2 = new Path(@"mydir\myfile");
+			Assert.IsFalse(path1.Equals(path2));
+			Assert.IsFalse(path1 == path2);
+		}
+
+		[Test]
+		public void OneWithTrailingSlash()
+		{
+			var path1 = new Path(@"mydir/mydir2");
+			var path2 = new Path(@"mydir/mydir2/");
+			Assert.IsTrue(path1.Equals(path2));
+			Assert.IsTrue(path1 == path2);
+		}
 	}
 }
